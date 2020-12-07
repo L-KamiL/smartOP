d5f0a34d83983eeb0137fd9b96141eeeb26318c6
Kamil <camil.lif@epitech.eu>
2020-12-07 09:52:13 +0100

    Reset repo

--- API/app/routes/intervention.route.js
+++
@@ -1,7 +0,0 @@
-module.exports = app => {
-    const intervention = require("../controllers/intervention.controller.js");
-
-    // Create a new intervention
-    app.post("/api/interventions", intervention.createIntervention);
-};
-
